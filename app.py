eb2c2e451d875b05a001c233abd7a20246d33dcb
yerlypedraza <yerlypedraza@gmail.com>
2024-06-11 17:57:40 -0500

    Actualizar app.py

--- app.py
+++ app.py
@@ -5,11 +5,8 @@ import streamlit as st
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 st.header('Análisis de Anuncios de Venta de Vehículos en EE.UU.') # Crear un encabezado para la aplicación
 hist_button = st.button('Construir histograma') # crear un botón
-
-# Crear casillas de verificación para seleccionar los gráficos
-build_histogram = st.checkbox('Construir un histograma')
-build_scatter = st.checkbox('Construir un gráfico de dispersión')
-        
+scatter_button = st.button('Construir gráfico de dispersión') # crear un botón
+    
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
