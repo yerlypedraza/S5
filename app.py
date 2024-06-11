c7f503619918d9cc19da7683cc5feb94b1692807
yerlypedraza <yerlypedraza@gmail.com>
2024-06-11 17:41:22 -0500

    Actualizar app.py con gráfico de dispersión

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ import plotly.express as px
 import streamlit as st
         
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+st.header('Análisis de Anuncios de Venta de Vehículos en EE.UU.') # Crear un encabezado para la aplicación
 hist_button = st.button('Construir histograma') # crear un botón
+
+# Crear casillas de verificación para seleccionar los gráficos
+build_histogram = st.checkbox('Construir un histograma')
+build_scatter = st.checkbox('Construir un gráfico de dispersión')
         
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
@@ -14,3 +19,13 @@ if hist_button: # al hacer clic en el botón
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+if build_scatter:
+    # Escribir un mensaje
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    
+    # Crear un gráfico de dispersión
+    fig_scatter = px.scatter(car_data, x="odometer", y="price")
+    
+    # Mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig_scatter, use_container_width=True)
